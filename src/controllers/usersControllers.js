eab2198913a7252c6fb9855b41a4d324a3bd46af
Daeram Chung <daeram.chung@gmail.com>
2022-06-03 22:58:16 +0900

    Setup login

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
 	return res.render("join.ejs", { pageTitle: "Join", errorMsg: null });
@@ -24,18 +25,50 @@ export const postJoin = async (req, res) => {
 		});
 	}
 
-	await User.create({
-		name,
-		email,
-		username,
-		password,
-		location,
-	});
-	return res.redirect("/login");
+	try {
+		await User.create({
+			name,
+			email,
+			username,
+			password,
+			location,
+		});
+		return res.redirect("/login");
+	} catch (err) {
+		return res.status(400).render("join", {
+			pageTitle,
+			errorMsg: err,
+		});
+	}
 };
 
-export const login = (req, res) => {
-	return res.send("login");
+export const getLogin = async (req, res) => {
+	return res.render("login", { pageTitle: "Log In", errorMsg: null });
+};
+
+export const postLogin = async (req, res) => {
+	const { username, password } = req.body;
+	const pageTitle = "Log In";
+
+	const user = await User.findOne({ username });
+	if (!user) {
+		return res.status(400).render("login", {
+			pageTitle,
+			errorMsg: "Sorry, we don't recognize this username.",
+		});
+	}
+
+	const isValidLogin = await bcrypt.compare(password, user.password);
+	if (!isValidLogin) {
+		return res.status(400).render("login", {
+			pageTitle,
+			errorMsg: "Username and password do not match.",
+		});
+	}
+
+	console.log("Log user in!");
+
+	return res.redirect("/");
 };
 
 export const logout = (req, res) => res.send("logout");
